68add28def9ebfbc1c4b44fe8805e05cec57215e
Hitesh Kumar <hitesh@singsys.com>
2019-08-16 17:02:39 +0530

    add Scope

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -8,22 +8,28 @@ export default Vue.extend({
     name: 'r-list',
     render: function (createElement) {
         let props = this.$attrs;
-        return createElement(this.tag, {
-            props
-        }, this.$scopedSlots.default({
-            items: this.items,
-            currentPage: this.currentPage,
-            previousPage: this.previousPage,
-            pageSize: this.pageSize,
-            total: this.total,
-            requesting: this.requesting,
-            switchPage: this.switchPage,
-            next: this.next,
-            filter: this.filter,
-            refresh: this.refresh,
-            changePageSize: this.changePageSize,
-            newIds: this.newIds
-        }))
+        let props = this.$attrs;
+        return createElement(
+            this.tag, 
+            { props }, 
+            [
+                this.$scopedSlots['lq.top'] ? this.$scopedSlots['lq.top']() : null,
+                this.$scopedSlots.default({
+                    items: this.items,
+                    currentPage: this.currentPage,
+                    previousPage: this.previousPage,
+                    pageSize: this.pageSize,
+                    total: this.total,
+                    requesting: this.requesting,
+                    switchPage: this.switchPage,
+                    next: this.next,
+                    filter: this.filter,
+                    refresh: this.refresh,
+                    changePageSize: this.changePageSize,
+                    newIds: this.newIds
+                })
+            ]
+        )
     },
     props: {
         requestMethod: {
