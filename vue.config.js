255273b7827d78ac739474e42ca9154c37e899ac
Hitesh Kumar <hitesh.kumar399@gmail.com>
2019-12-20 23:28:43 +0530

    fixed config

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,19 @@
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    configureWebpack: config => {
-	    config.externals = {
-	       ...config.externals,
-	      'axios': 'axios',
-	      'validate.js': 'validate.js',
-	      'vuex': 'vuex',
-	      'vuejs-object-helper': 'vuejs-object-helper',
-	      'vue': 'vue',
-	      'lodash/cloneDeep': 'lodash/cloneDeep',
-	      'lodash/core': 'lodash/core',
-	    }
+	configureWebpack: config => {
+		const externalPackages = {
+			'axios': 'axios',
+			'validate.js': 'validate.js',
+			'vuex': 'vuex',
+			'vuejs-object-helper': 'vuejs-object-helper',
+			'vue': 'vue',
+			'lodash/cloneDeep': 'lodash/cloneDeep',
+			'lodash/core': 'lodash/core',
+		}
+		config.externals = {
+			...config.externals,
+			...(process.env.NODE_ENV === 'production' ? externalPackages : {})
+		}
 	}
- }
+}
