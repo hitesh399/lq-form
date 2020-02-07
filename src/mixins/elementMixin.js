755eb3e03701c2c49d6eb489f9c5a5ca1a7a8de9
Hitesh Kumar <hitesh.kumar399@gmail.com>
2020-02-07 12:19:41 +0530

    Update elementMixin.js

--- src/mixins/elementMixin.js
+++ src/mixins/elementMixin.js
@@ -148,6 +148,7 @@ const formElementMix = {
         this.$lqForm.addProps(this.formName, this.id, {
             touch: false,
             test: this.validate,
+            formatter: null,
             dirty: false
         });
     },
